0d6b439efe0952040d1607e81f835ca1c1f06b93
Ignacio Moraga <ignacio.moraga.pineda@ciisa.cl>
2022-10-10 17:45:44 -0300

    cambios varios

--- app/src/main/java/com/example/apppokemon/HomeLayout.kt
+++ app/src/main/java/com/example/apppokemon/HomeLayout.kt
@@ -1,11 +1,22 @@
 package com.example.apppokemon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class HomeLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_layout)
+
+        val btn_tv_pokedex = findViewById<TextView>(R.id.act_home_layout_tv_pokedex)
+
+        btn_tv_pokedex.setOnClickListener {
+            val intent = Intent(this, DexLayout::class.java)
+            startActivity(intent)
+        }
+
     }
 }
