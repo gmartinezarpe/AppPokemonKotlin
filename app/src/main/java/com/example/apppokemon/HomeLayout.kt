acf269f9f5d78ee3dc9948fce71c41025a832917
Ignacio Moraga <ignacio.moraga.pineda@ciisa.cl>
2022-10-10 22:38:50 -0300

    layout update-delete

--- app/src/main/java/com/example/apppokemon/HomeLayout.kt
+++ app/src/main/java/com/example/apppokemon/HomeLayout.kt
@@ -12,9 +12,27 @@ class HomeLayout : AppCompatActivity() {
         setContentView(R.layout.activity_home_layout)
 
         val btn_tv_pokedex = findViewById<TextView>(R.id.act_home_layout_tv_pokedex)
+        val btn_tv_teams = findViewById<TextView>(R.id.act_home_layout_tv_teams)
+        val btn_tv_update = findViewById<TextView>(R.id.act_home_layout_tv_update)
+        val btn_tv_delete = findViewById<TextView>(R.id.act_home_layout_tv_delete)
 
         btn_tv_pokedex.setOnClickListener {
             val intent = Intent(this, DexLayout::class.java)
+            startActivity(intent)
+        }
+
+        btn_tv_teams.setOnClickListener {
+            val intent = Intent(this, grid_view_items::class.java)
+            startActivity(intent)
+        }
+
+        btn_tv_update.setOnClickListener {
+            val intent = Intent(this, UpdataActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_tv_delete.setOnClickListener {
+            val intent = Intent(this, DeleteActivity::class.java)
             startActivity(intent)
         }
 
